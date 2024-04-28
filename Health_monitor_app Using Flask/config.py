04596bf0013c507477fefe0e8b9de8c636aeb965
Smitraj <smitrajbankar11@gmail.com>
2024-04-28 23:06:38 +0530

    Flask Deleted

--- Health_monitor_app Using Flask/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
